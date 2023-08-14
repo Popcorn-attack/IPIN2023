670e099a25b3c116dc64c4edf0b1d671cdd5e957
vanylFM <34267468+vanylFM@users.noreply.github.com>
2023-08-14 15:39:53 +0100

    Update peak_detection_using_Acce.py

--- peak_detection_using_Acce.py
+++ peak_detection_using_Acce.py
@@ -1,1 +1,41 @@
+def StepDetection_for_whole_data(Accedata):
+    #首先对数据进行低通滤波处理
+    acce_data = np.array(Accedata)
+    acce_x, acce_y, acce_z = acce_data[:, 2], acce_data[:, 3], acce_data[:, 4]
+    acce_sqrt1 = (np.sqrt((acce_x ** 2 + acce_y ** 2 + acce_z ** 2)))
 
+    b, a = signal.butter(8, 0.1, 'low')
+    filtered = signal.filtfilt(b, a, acce_sqrt1)
+
+    #然后对低通滤波后的数据进行波峰波谷检测
+    peak_of_filter, _ = signal.find_peaks(filtered, distance=30, prominence=1.5) # 波峰位置
+    valley_of_filter, _ = signal.find_peaks(-filtered, distance=30, prominence=1.5) # 波谷位置
+
+    plt.figure()
+    plt.plot(filtered)
+    plt.plot(peak_of_filter, filtered[peak_of_filter], "x")
+    plt.show()
+
+    peak_valley = np.concatenate((peak_of_filter, valley_of_filter)) #将波峰波谷位置合并
+    peak_valley.sort() # 排序
+
+    '''
+    判断step的方法是： 先找到一个波谷，波谷前面必然是一个波峰，且距离应该在0.5Hz*simpleRate范围内(半步之内)
+    如果找到一个波谷，距离前面一个波峰太远，则成为该步数为一个无效步数。
+    从方法对于实时数据与离线数据都适用
+    '''
+    valley_TRUE = []
+    peak_TRUE = []
+    for i in range(len(peak_valley)):
+        #先找到一个波谷
+        if peak_valley[i] in peak_of_filter:
+            continue
+
+        if i == 0:
+            continue
+
+        # 此时peak_valley[i]为波谷
+        if peak_valley[i-1] in peak_of_filter and (peak_valley[i] - peak_valley[i-1]) < 40:
+            peak_TRUE.append(peak_valley[i-1])
+            valley_TRUE.append(peak_valley[i])
+    return peak_TRUE, valley_TRUE
